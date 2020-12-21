3a68f11b0bfbb3fb422e602e238126aa41e27725
Stefano1991 <pieronistefano@hotmail.com>
2020-12-21 03:00:00 -0300

    Project Start

---
+++ Assets/Scripts/FarmScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmScript : MonoBehaviour
+{
+    public int curWorkers;
+    private int maxWorkers;
+
+    public int baseFood;
+
+    private void Update()
+    {
+       if(curWorkers != 0)
+        {
+            StartCoroutine(AddFood(5, baseFood*curWorkers));
+        }
+    }
+
+
+    IEnumerator AddFood(int x, int t)
+    {
+        yield return new WaitForSeconds(t);
+
+        GameManager.instance.UpdateFood(x);
+
+    }
+
+
+    public void AddWorker()
+    {
+        if(GameManager.instance.villagers != 0 && GameManager.instance.freeVillagers != 0)
+        {
+            GameManager.instance.villagers -= 1;
+            curWorkers += 1;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    #region Resources
+    [SerializeField]
+    int maxFood = 100; 
+    [SerializeField]
+    int maxWood = 100;
+
+    public int currentFood;
+    public int currentWood;
+    public int villagers;
+    public int freeVillagers;
+
+    #endregion
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentFood >= maxFood)
+        {
+            currentFood = maxFood;
+        }
+        if(currentWood >= maxWood)
+        {
+            currentWood = maxWood;
+        }
+    }
+
+
+    public void UpdateWood(int x)
+    {
+        currentWood += x;
+    }
+
+    public void UpdateFood(int x)
+    {
+        currentFood += x;
+    }
+}
